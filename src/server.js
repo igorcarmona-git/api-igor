366dc11bfd8d9d361e57f56b848f6deb7758bf7b
Igor Carmona <igorcarmonawork@gmail.com>
2024-03-19 23:41:17 -0300

    chore: Added cors to the host to be avaialable to localhost ports and receive http requests from anyone

--- src/server.js
+++ src/server.js
@@ -1,11 +1,18 @@
 const express = require("express");
+const cors = require('cors');
 const routerPerson = require('../src/routes/personRoute');
 const APP = express();
 const PORT = 3003;
 const db = require('./infra/db');
 
-db.sync(); //tem que colocar esse db.sync() para sincronizar os dados com o banco
-APP.use(express.json()); // forma de se fazer middlewares, quando a requisição chega, como ele trata.
+//tem que colocar esse db.sync() para sincronizar os dados com o banco
+db.sync(); 
+
+// forma de se fazer middlewares, quando a requisição chega, como ele trata.
+APP.use(express.json()); 
+
+// cors -->  Você está permitindo que qualquer origem (qualquer domínio) tenha acesso aos recursos da sua API. O asterisco (*) indica que não há restrições de origem, ou seja, qualquer origem pode fazer requisições para a sua API.
+APP.use(cors( {origin: '*'} )); 
 
 try{
     APP.use('/people', routerPerson);
